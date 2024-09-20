148f9b8369bc143e3a683e153b2da51832c8fb0b
polymathLTE <lawaltemmanuel@gmail.com>
2024-09-20 11:00:08 +0100

    Update Homepage.jsx

--- src/app/components/Homepage.jsx
+++ src/app/components/Homepage.jsx
@@ -14,16 +14,22 @@ export default function Homepage() {
                     To assess individual learning preferences across various dimensions
                 </h3>
                 <h3 className='text-2xl'>
-                    By Akinwole Pelumi Emmanuel
+                    By Computer Applications 
                 </h3>
                 <h3 className='text-2xl'>
-                    IFT/17/2415
+                    ENT GROUP
                 </h3>
                 <h3 className='text-2xl'>
                     Supervised By
                 </h3>
+                <h3 className='text-2x1'>
+                    Mr. Ogunbodede
+                </h3>
                 <h3 className='text-2xl'>
-                    Prof. (Mrs.) O. K. Boyinbode
+                    &
+                </h3>
+                <h3 className='text-2xl'>
+                    Mrs. Victoria Omoniyi
                 </h3>
                 <Link
                     className='bg-slate-700 px-6 py-4 text-lg text-white rounded-lg'
